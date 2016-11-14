c68fc3270b043292da8d42e035bffcdc60971599
MichaelKevinKelly <m.i.k.kelly7@gmail.com>
2016-11-14 15:35:27 -0500

    Update Motion.cs

--- C#/Motion.cs
+++ C#/Motion.cs
@@ -1,6 +1,6 @@
 /*
- * Attach this script to the main camera to cause it to continuously 
- * move in the direction of the user's gaze
+ * Attach this script to the main camera to allow it to 
+ * move in the direction of the user's gaze. Motion can be toggled on and off.
  * 
  */
 
