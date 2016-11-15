9f6312cafb9f2691311d1213593e80366f9181c1
MichaelKevinKelly <m.i.k.kelly7@gmail.com>
2016-11-14 20:15:34 -0500

    Update LifeScript.cs

--- C#/LifeScript.cs
+++ C#/LifeScript.cs
@@ -2,7 +2,7 @@
  * 
  * Simple implementation of the Game of Life in 3D using Unity.
  * To use this script, attach to an empty GameObject in a Unity scene. Set prefab to cube 
- * GameObject in the Inspector panel. The square object should be scaled approximately 
+ * GameObject in the Inspector panel. The cube object should be scaled approximately 
  * by the cell size dimension.
  * 
  **/
